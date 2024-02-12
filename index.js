fd6d992efaf28a79a9a2f24586091b7240d0e3e3
non-reai <63729314+non-reai@users.noreply.github.com>
2024-02-12 01:51:45 +0000

    added __dirname

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import fetch from 'node-fetch'
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-app.use("/", express.static('public'));
+app.use("/", express.static(join(__dirname,'public')));
 
 app.use(express.json())
 
