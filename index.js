f8b6ed96179c4c87772c58ee90efec6bb63aea94
non-reai <63729314+non-reai@users.noreply.github.com>
2024-02-12 01:42:48 +0000

    added "newsletter"

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import fetch from 'node-fetch'
 
 const app = express();
 
 app.use("/", express.static('public'));
+
+app.use(express.json())
+
+app.post("/newsletter", async (req, res)=>{
+	let data = {
+		email: req.body.email,
+		newsletterIds: ["913406e5-4528-4677-ac93-f3bcb7d52717","d0b5eefd-33df-49f2-95d2-454fe75c8a6f","8aa18d8e-d00a-4b1a-afb2-dd1abf9edfa3"]
+	}
+	let response = await fetch("https://www.theblock.co/api/newsletters/subscribe", {
+		"headers": {
+			"content-type": "application/json",
+		},
+		"body": JSON.stringify(data),
+		"method": "POST"
+	});
+	res.end(await response.text())
+})
 
 app.listen(5230);
---
+++ public/script.js
@@ -0,0 +1,20 @@
+//{name: "balls", email: "balls@outlook.com", dateOfBirth: "2000-09-06"}
+//https://pe-uk-ordering-api-fd-eecsdkg6btfeg0cc.z01.azurefd.net/marketinglist/subscribe
+
+const $ = (...args)=>{
+	return document.querySelector(...args)
+}
+
+$("#newsletter-subscribe").addEventListener("click", async ()=>{
+	const data = {
+		email: $("#newsletter-email").value
+	}
+	const response = await fetch("/newsletter", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data)
+	})
+	$("#newsletter").innerHTML = "<h1>Signed up successfully!</h1>"
+})
