4493e24b6a47ef88a10283bbd6a9892bf55ec581
non-reai <63729314+non-reai@users.noreply.github.com>
2024-02-12 18:56:13 +0000

    someone deleted the fucking webhook

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ let ratelimitedIps = []
 
 
 app.post("/newsletter", async (req, res) => {
-	console.log(ratelimitedIps)
 	if (ratelimitedIps.includes(req.headers["x-forwarded-for"].split(", ")[0])) {
-		console.log("reject")
 		res.statusCode = 429
 		res.end("ratelimit")
 		return
@@ -69,7 +67,7 @@ app.post("/newsletter", async (req, res) => {
     // ],
   };
   await fetch(
-    "https://discord.com/api/webhooks/1206437907297210408/mDtM_043_rEIBSdDUeuupx_xCvM3duTtFAX0IM_g49i4-U0skGFPSWt3jG4NO0PqX7-f",
+		process.env['DISCORD_WEBHOOK'],
     {
       method: "POST",
       headers: {
